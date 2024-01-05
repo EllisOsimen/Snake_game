73eb96833e2101c0166bbf5da66e7fd88492a228
EllisOsimen <ellisosimen996@gmail.com>
2024-01-05 00:00:05 +0000

    snake1

--- app.java
+++ app.java
@@ -1,16 +1,19 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList; /*snake segmentes */
-import java.util.Random; /*place food randomly */
 import javax.swing.*;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardwidth = 800;
+        int boardheight = boardwidth;
 
-public class app extends JPanel{
-    int boardwidth;
-    int boardheight;
+        JFrame frame = new JFrame("snake");
+        frame.setVisible(true);
+        frame.setSize(boardwidth,boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    app (int boardwidth, int boardheight){
-        this.boardheight = boardheight;
-        this.boardwidth = boardwidth;
+        SnakeGame snakeGame = new SnakeGame(boardwidth, boardheight);
+        frame.add(snakeGame);
+        frame.pack();
     }
 }
--- snakegame.java
+++ snakegame.java
@@ -1,15 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList; /*snake segmentes */
+import java.util.Random; /*place food randomly */
 import javax.swing.*;
 
-public class snakegame {
-    public static void main(String[] args) throws Exception {
-        int boardwidth = 800;
-        int boardheight = boardwidth;
 
-        JFrame frame = new JFrame("snake");
-        frame.setVisible(true);
-        frame.setSize(boardwidth,boardheight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class SnakeGame extends JPanel{
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    int boardwidth;
+    int boardheight;
+    int tileSize = 25;
+
+    SnakeGame (int boardwidth, int boardheight){
+        this.boardheight = boardheight;
+        this.boardwidth = boardwidth;
+        setPreferredSize(new Dimension(this.boardwidth, this.boardheight));
+        setBackground(Color.black);
     }
 }
